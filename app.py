0a23505286d054aa438a895e039ccb91471a8af1
Obcolek <133579571+Obcolek@users.noreply.github.com>
2023-06-18 14:46:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
-"""
-sdsd
-"""
 from flask import Flask
-
 app = Flask(__name__)
-
-
 @app.route('/')
 def index():
-    """
-    sdsd
-    """
-    return '<h1>Hello WSB! Greetings from Flask!</h1>'
-
-
+ return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>'
 if __name__ == "__main__":
-    app.run(debug=True)
+ app.run(debug=True)
